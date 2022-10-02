99a7e9e6c7fcd64c20f4625d17e8daead3489101
Zhell <50169243+krbz999@users.noreply.github.com>
2022-10-02 14:02:58 +0200

    10.0.1
    Add missing localization.
    Fix 'Infinite duration' badge never showing.

--- setup.js
+++ setup.js
@@ -4,27 +4,28 @@ import { registerSettings } from "./scripts/settings.mjs";
 import { VisualEffects } from "./scripts/visual-active-effects.mjs";
 
 Hooks.once("init", () => {
-    console.log("ZHELL | Initializing Visual Active Effects");
+  console.log("ZHELL | Initializing Visual Active Effects");
 });
 
 Hooks.once("setup", registerSettings);
 
-Hooks.once("ready", async function(){
-    registerHelpers();
-    const panel = new VisualEffects();
-    await panel.render(true);
-    Hooks.on("collapseSidebar", panel.handleExpand.bind(panel));
-    
-    for ( const hook of [
-        "updateWorldTime", "createActiveEffect",
-        "updateActiveEffect", "deleteActiveEffect",
-        "controlToken", "preUpdateToken"
-    ] ) {
-        Hooks.on(hook, () => panel.refresh());
-    }
+Hooks.once("ready", async function () {
+  registerHelpers();
+  const panel = new VisualEffects();
+  await panel.render(true);
+  Hooks.on("collapseSidebar", panel.handleExpand.bind(panel));
 
-    collapsibleSetup();
+  for (const hook of [
+    "updateWorldTime", "createActiveEffect",
+    "updateActiveEffect", "deleteActiveEffect",
+    "controlToken", "preUpdateToken"
+  ]) {
+    Hooks.on(hook, () => panel.refresh());
+  }
 
-    const pixels = game.settings.get(MODULE, ICON_SIZE) ?? 50;
-    document.documentElement.style.setProperty("--visual-active-effects-icon-size", `${pixels}px`);
+  collapsibleSetup();
+
+  const pixels = game.settings.get(MODULE, ICON_SIZE) ?? 50;
+  const property = "--visual-active-effects-icon-size";
+  document.documentElement.style.setProperty(property, `${pixels}px`);
 });
